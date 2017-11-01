Ibochkarev/revolution

commit 1eeb5a97637bf7a9a732ffa4b459af10783899d4

Let's create a new "please wait" windows on any package download

manager/assets/modext/workspace/package.containers.js
@@ -85,7 +85,7 @@ Ext.extend(MODx.panel.Packages,MODx.Panel,{
         var r;
         var g = Ext.getCmp('modx-package-grid');
         if (!g) return false;
-        
+
         if (va.signature != undefined && va.signature != '') {
             r = {signature: va.signature};
         } else {
@@ -222,8 +222,10 @@ Ext.extend(MODx.panel.PackagesBrowser,MODx.Panel,{
 		this.wait.show();
 	}
 
-	,hideWait: function(){
-		this.wait.hide();
-	}
+    ,hideWait: function() {
+        if (this.wait) {
+            this.wait.destroy();
+        }
+    }
 });
 Ext.reg('modx-panel-packages-browser',MODx.panel.PackagesBrowser);

commit 09fe75344944bc6766f65b198a3fea7948f553a9

Fixed issue introduced by #13506 preventing to download 2 packages in a row

manager/assets/modext/workspace/package.containers.js
@@ -217,16 +217,17 @@ Ext.extend(MODx.panel.PackagesBrowser,MODx.Panel,{
 		Ext.getCmp('packages-breadcrumbs').updateDetail(bd);
 	}
 
-	,showWait: function(){
-		if(typeof(this.wait) == "undefined"){
-			this.wait = new MODx.PackageBrowserWaitWindow();
-		}
-		this.wait.show();
-	}
+    ,showWait: function(){
+        if (!this.wait) {
+            this.wait = new MODx.PackageBrowserWaitWindow();
+        }
+        this.wait.show();
+    }
 
     ,hideWait: function() {
         if (this.wait) {
             this.wait.destroy();
+            delete this.wait;
         }
     }
 });