Ibochkarev/revolution

commit 0e13111fbc5303e61e8fbe4b2f09e66049d5cac8

fix(Resource) delete dialog UI

manager/assets/modext/sections/resource/update.js
@@ -70,7 +70,8 @@ Ext.extend(MODx.page.UpdateResource,MODx.Component,{
 
     ,deleteResource: function(btn,e) {
         MODx.msg.confirm({
-            text: _('resource_delete_confirm')
+            title: this.config.pagetitle ? _('resource_delete') + ' ' + this.config.pagetitle + ' (' + this.config.resource + ')' : _('resource_delete')
+            ,text: _('resource_delete_confirm')
             ,url: MODx.config.connector_url
             ,params: {
                 action: 'resource/delete'
@@ -103,7 +104,7 @@ Ext.extend(MODx.page.UpdateResource,MODx.Component,{
 
     ,getButtons: function(cfg) {
         var btns = [];
-        
+
         btns.push({
             text: cfg.lockedText || _('locked')
             ,id: 'modx-abtn-locked'
@@ -125,7 +126,7 @@ Ext.extend(MODx.page.UpdateResource,MODx.Component,{
                 ,ctrl: true
             }]
         });
-	    
+
         if (cfg.canDuplicate == 1 && (cfg.record.parent !== parseInt(MODx.config.tree_root_id) || cfg.canCreateRoot == 1)) {
             btns.push({
                 text: _('duplicate')

manager/controllers/default/resource/update.class.php
@@ -42,6 +42,7 @@ class ResourceUpdateManagerController extends ResourceManagerController {
             MODx.load({
                 xtype: "modx-page-resource-update"
                 ,resource: "'.$this->resource->get('id').'"
+                ,pagetitle: "'.$this->resource->get('pagetitle').'"
                 ,record: '.$this->modx->toJSON($this->resourceArray).'
                 ,publish_document: "'.$this->canPublish.'"
                 ,preview_url: "'.$this->previewUrl.'"

manager/assets/modext/widgets/resource/modx.tree.resource.js
@@ -206,8 +206,9 @@ Ext.extend(MODx.tree.Resource,MODx.tree.Tree,{
     ,deleteDocument: function(itm,e) {
         var node = this.cm.activeNode;
         var id = node.id.split('_');id = id[1];
+        var pagetitle = node.ui.textNode.innerText;
         MODx.msg.confirm({
-            title: _('resource_delete')
+            title: pagetitle ? _('resource_delete') + ' ' + pagetitle : _('resource_delete')
             ,text: _('resource_delete_confirm')
             ,url: MODx.config.connector_url
             ,params: {

commit 53e179a667ba6dc3d853d491aa1019e96ea88c8d

Fix javascript issue on resources containing a quote sign - Introduced in #13497

manager/assets/modext/sections/resource/update.js
@@ -70,7 +70,7 @@ Ext.extend(MODx.page.UpdateResource,MODx.Component,{
 
     ,deleteResource: function(btn,e) {
         MODx.msg.confirm({
-            title: this.config.pagetitle ? _('resource_delete') + ' ' + this.config.pagetitle + ' (' + this.config.resource + ')' : _('resource_delete')
+            title: this.config.record.pagetitle ? _('resource_delete') + ' ' + this.config.record.pagetitle + ' (' + this.config.resource + ')' : _('resource_delete')
             ,text: _('resource_delete_confirm')
             ,url: MODx.config.connector_url
             ,params: {

manager/controllers/default/resource/update.class.php
@@ -42,7 +42,6 @@ class ResourceUpdateManagerController extends ResourceManagerController {
             MODx.load({
                 xtype: "modx-page-resource-update"
                 ,resource: "'.$this->resource->get('id').'"
-                ,pagetitle: "'.$this->resource->get('pagetitle').'"
                 ,record: '.$this->modx->toJSON($this->resourceArray).'
                 ,publish_document: "'.$this->canPublish.'"
                 ,preview_url: "'.$this->previewUrl.'"